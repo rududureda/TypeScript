d05e58a48b67f89ea0968afa74297680b9117f3d
rududureda <ranc.reda@gmail.com>
2024-03-04 11:45:32 +0200

    First time adding TS

---
+++ introduction/public/js/index.js
@@ -0,0 +1,47 @@
+"use strict";
+let age = 29;
+let firstName = 'Reda';
+let itFake = false;
+age = 30;
+firstName = 'ona';
+itFake = true;
+let timeLeft = [30, 40, 50, 32];
+let title = ['Reda is the best', 'Reda is great', 'Reda is smart'];
+timeLeft.push(22, 25);
+title.push('Reda is not crazy');
+console.log(title);
+let user = {
+    logInName: 'Reda',
+    id: 22,
+    logIn: true,
+};
+user.logInName = 'Ana';
+user.id = 5552;
+console.log(user);
+function suma(a, b) {
+    return a + b;
+}
+const mathematic = (a, b) => {
+    return a - b;
+};
+console.log('suma', suma(2, 5));
+console.log('mathematic', mathematic(2, 2));
+function mathematicReduce(item) {
+    const result = item.reduce((a, b) => a + b, 0);
+    console.log(result);
+    return result;
+}
+mathematicReduce([1, 2, 3, 4]);
+const firstFlower = {
+    isBlum: false,
+    title: 'snake plant',
+    color: 'green',
+    costumers: ['ana', 'john'],
+    plantFamily: { size: 'big', lith: false },
+};
+function buyFlower(flower) {
+    console.log(`Buy new plant ${flower.title}  is very ${flower.plantFamily.size}`);
+}
+buyFlower(firstFlower);
+console.log('veikia');
+//# sourceMappingURL=index.js.map
---
+++ introduction/src/index.ts
@@ -0,0 +1,82 @@
+let age: number = 29;
+let firstName: string = 'Reda';
+let itFake: boolean = false;
+
+age = 30;
+firstName = 'ona';
+itFake = true;
+
+//MASYVAI//
+
+let timeLeft: number[] = [30, 40, 50, 32];
+let title: string[] = ['Reda is the best', 'Reda is great', 'Reda is smart'];
+
+timeLeft.push(22, 25);
+title.push('Reda is not crazy');
+
+console.log(title);
+
+//Objektai//
+
+let user: { logInName: string; id: number; logIn: boolean } = {
+  logInName: 'Reda',
+  id: 22,
+  logIn: true,
+};
+user.logInName = 'Ana';
+user.id = 5552;
+
+console.log(user);
+
+//FUNKCIJOS//
+
+function suma(a: number, b: number): number {
+  return a + b;
+}
+
+const mathematic = (a: number, b: number): number => {
+  return a - b;
+};
+
+console.log('suma', suma(2, 5));
+console.log('mathematic', mathematic(2, 2));
+
+function mathematicReduce(item: number[]) {
+  const result = item.reduce((a, b) => a + b, 0);
+  console.log(result);
+  return result;
+}
+
+mathematicReduce([1, 2, 3, 4]);
+
+//INTERFACES//
+interface Plant {
+  size: string;
+  lith: boolean;
+}
+
+interface Flowers {
+  title: string;
+  color: string;
+  isBlum: boolean;
+  costumers: string[];
+  plantFamily: Plant;
+}
+
+const firstFlower: Flowers = {
+  isBlum: false,
+  title: 'snake plant',
+  color: 'green',
+  costumers: ['ana', 'john'],
+  plantFamily: { size: 'big', lith: false },
+};
+
+function buyFlower(flower: Flowers): void {
+  console.log(
+    `Buy new plant ${flower.title}  is very ${flower.plantFamily.size}`
+  );
+}
+buyFlower(firstFlower);
+console.log('veikia');
+
+
